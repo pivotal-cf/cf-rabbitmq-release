30dfb23cefd77d780e4db4952e1b271394eb3f4f
Alex Ley <aley@pivotal.io>
2015-01-28 10:06:17 +0000

    Revert "allow the migration tool to run"
    This reverts commit 4268b9e76a322ae9f873687b5f53aa8e776e5546.

--- src/rabbitmq-cluster-migration-tool/main.go
+++ src/rabbitmq-cluster-migration-tool/main.go
@@ -1,17 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/pivotal-cf/cf-rabbit-release/src/rabbitmq-cluster-migration-tool/configuration"
-)
-
 func main() {
-	migrator := configuration.NewMigrator("")
-	err := migrator.MigrateConfiguration()
-
-	if err != nil {
-		fmt.Printf("Finished with error: %s", err)
-	}
 
 }
