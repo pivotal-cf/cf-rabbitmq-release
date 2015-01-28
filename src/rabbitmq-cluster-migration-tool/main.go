4268b9e76a322ae9f873687b5f53aa8e776e5546
Alex Ley <aley@pivotal.io>
2015-01-28 09:41:48 +0000

    allow the migration tool to run
    Signed-off-by: Nadia Odunayo <nodunayo@pivotal.io>

--- src/rabbitmq-cluster-migration-tool/main.go
+++ src/rabbitmq-cluster-migration-tool/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/pivotal-cf/cf-rabbit-release/src/rabbitmq-cluster-migration-tool/configuration"
+)
+
 func main() {
+	migrator := configuration.NewMigrator("")
+	err := migrator.MigrateConfiguration()
+
+	if err != nil {
+		fmt.Printf("Finished with error: %s", err)
+	}
 
 }
