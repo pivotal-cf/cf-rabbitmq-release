39a17c3d25473a42a4df862123fe8b485f131615
Craig Furman <cfurman@pivotal.io>
2015-11-05 15:27:43 +0000

    call stop_app when new minor version of rabbit
    Signed-off-by: William Martin <william.la.martin@gmail.com>

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -1,7 +1,10 @@
 package integration_test
 
 import (
+	"io/ioutil"
+	"os"
 	"os/exec"
+	"path/filepath"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -20,7 +23,22 @@ var _ = Describe("Upgrading RabbitMQ", func() {
 	var (
 		args    []string
 		session *gexec.Session
+
+		tmpDir  string
+		tmpFile string
 	)
+
+	itExitsWithZero := func() {
+		It("exits with a zero exit code", func() {
+			Eventually(session).Should(gexec.Exit(0))
+		})
+	}
+
+	itExitsWithNonZero := func() {
+		It("exits with a non-zero exit code", func() {
+			Expect(session.ExitCode()).NotTo(BeZero())
+		})
+	}
 
 	JustBeforeEach(func() {
 		session = execBin(args...)
@@ -28,16 +46,81 @@ var _ = Describe("Upgrading RabbitMQ", func() {
 
 	BeforeEach(func() {
 		args = []string{
-			"-rabbitmqctl-path", "/tmp/rabbitmqctl",
+			"-rabbitmqctl-path", "/idontexist/rabbitmqctl",
 			"-node", "node",
 			"-new-rabbitmq-version", "0.0.1",
 		}
+
+		var err error
+		tmpDir, err = ioutil.TempDir("", "rabbitmq-upgrade-tests")
+		Expect(err).NotTo(HaveOccurred())
+		tmpFile = filepath.Join(tmpDir, "dummy.txt")
+
+		os.Setenv("TEST_OUTPUT_FILE", tmpFile)
+	})
+
+	AfterEach(func() {
+		Expect(os.RemoveAll(tmpDir)).To(Succeed())
 	})
 
 	Context("When there is no new version of rabbit", func() {
-		It("exits with a zero exit code", func() {
-			session := execBin(args...)
-			Eventually(session).Should(gexec.Exit(0))
+		BeforeEach(func() {
+			cwd, err := os.Getwd()
+			Expect(err).NotTo(HaveOccurred())
+			args = []string{
+				"-rabbitmqctl-path", filepath.Join(cwd, "test-assets", "rabbitmqctl-dummy.sh"),
+				"-node", "my-node",
+				"-new-rabbitmq-version", "3.4.3.1",
+			}
+		})
+
+		itExitsWithZero()
+
+		It("doesn't call stop app", func() {
+			_, err := os.Stat(tmpFile)
+			Expect(os.IsNotExist(err)).To(BeTrue())
+		})
+	})
+
+	Context("when there is a new patch version of rabbit", func() {
+		BeforeEach(func() {
+			cwd, err := os.Getwd()
+			Expect(err).NotTo(HaveOccurred())
+			args = []string{
+				"-rabbitmqctl-path", filepath.Join(cwd, "test-assets", "rabbitmqctl-dummy.sh"),
+				"-node", "my-node",
+				"-new-rabbitmq-version", "3.4.4.1",
+			}
+		})
+
+		itExitsWithZero()
+
+		It("doesn't call stop app", func() {
+			_, err := os.Stat(tmpFile)
+			Expect(os.IsNotExist(err)).To(BeTrue())
+		})
+	})
+
+	Context("when there is a new minor version of rabbit", func() {
+		BeforeEach(func() {
+			cwd, err := os.Getwd()
+			Expect(err).NotTo(HaveOccurred())
+
+			args = []string{
+				"-rabbitmqctl-path", filepath.Join(cwd, "test-assets", "rabbitmqctl-dummy.sh"),
+				"-node", "my-node",
+				"-new-rabbitmq-version", "3.5.6",
+			}
+		})
+
+		itExitsWithZero()
+
+		It("calls stop app", func() {
+			Eventually(session).Should(gexec.Exit())
+
+			contents, err := ioutil.ReadFile(tmpFile)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(contents).To(Equal([]byte("-n my-node\n")))
 		})
 	})
 
@@ -49,10 +132,7 @@ var _ = Describe("Upgrading RabbitMQ", func() {
 			}
 		})
 
-		It("exits with a non-zero exit code", func() {
-			Eventually(session).Should(gexec.Exit())
-			Expect(session.ExitCode()).NotTo(BeZero())
-		})
+		itExitsWithNonZero()
 
 		It("provides a meaningful error", func() {
 			Eventually(session.Err).Should(gbytes.Say("Missing -rabbitmqctl-path flag"))
@@ -67,10 +147,7 @@ var _ = Describe("Upgrading RabbitMQ", func() {
 			}
 		})
 
-		It("exits with a non-zero exit code", func() {
-			Eventually(session).Should(gexec.Exit())
-			Expect(session.ExitCode()).NotTo(BeZero())
-		})
+		itExitsWithNonZero()
 
 		It("provides a meaningful error", func() {
 			Eventually(session.Err).Should(gbytes.Say("Missing -node flag"))
@@ -85,10 +162,7 @@ var _ = Describe("Upgrading RabbitMQ", func() {
 			}
 		})
 
-		It("exits with a non-zero exit code", func() {
-			Eventually(session).Should(gexec.Exit())
-			Expect(session.ExitCode()).NotTo(BeZero())
-		})
+		itExitsWithNonZero()
 
 		It("provides a meaningful error", func() {
 			Eventually(session.Err).Should(gbytes.Say("Missing -new-rabbitmq-version flag"))
--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"flag"
 	"log"
+	"os/exec"
+	"regexp"
+	"strings"
 )
 
 func main() {
@@ -16,10 +19,41 @@ func main() {
 	assertFlag(*rabbitmqctlPath, "rabbitmqctl-path")
 	assertFlag(*node, "node")
 	assertFlag(*newRabbitmqVersion, "new-rabbitmq-version")
+
+	rabbitmqctlStatusCommand := exec.Command(*rabbitmqctlPath, "status", "-n", *node)
+
+	out, err := rabbitmqctlStatusCommand.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	rabbitMQVersionLine := findRabbitMQVersionLine(out)
+	regex := regexp.MustCompile(`^\{rabbit,"RabbitMQ","(.*)"\},$`)
+	rabbitMQVersion := regex.FindAllStringSubmatch(rabbitMQVersionLine, -1)[0][1]
+
+	newVersionComponents := strings.Split(*newRabbitmqVersion, ".")
+	remoteVersionComponents := strings.Split(rabbitMQVersion, ".")
+
+	if newVersionComponents[1] != remoteVersionComponents[1] {
+		if err := exec.Command(*rabbitmqctlPath, "stop_app", "-n", *node).Run(); err != nil {
+			panic(err)
+		}
+	}
 }
 
 func assertFlag(flag, name string) {
 	if flag == "" {
 		log.Fatalf("Missing -%s flag\n", name)
 	}
+}
+
+func findRabbitMQVersionLine(out []byte) string {
+	lines := strings.Split(string(out), "\n")
+	for _, line := range lines {
+		if strings.Contains(line, "{rabbit,") {
+			return strings.TrimSpace(line)
+		}
+	}
+
+	return ""
 }
