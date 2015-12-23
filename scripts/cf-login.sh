fa3bf5a401e6b5482484b5d4d6c7847e10e8788a
Simon Jones <simon@cloudcredo.com>
2015-12-23 09:40:18 +0000

    Fix Rakefile, remove unused scripts
    [#110559322]
    
    Signed-off-by: Tiago Scolari <tiago@cloudcredo.com>

--- scripts/cf-login.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# this assumes CF is deployed to a local VM created by bosh-lite:
-# https://github.com/cloudfoundry/bosh-lite
-cf api http://api.bosh-lite.com
-cf auth admin admin && cf create-org pcf-rabbitmq && cf target -o pcf-rabbitmq && cf create-space dev && cf target -o pcf-rabbitmq -s dev
