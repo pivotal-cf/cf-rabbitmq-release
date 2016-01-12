6c29a445b2478fdb33d6f471eae6c913c24aa771
William Martin <wmartin@pivotal.io>
2016-01-12 17:58:12 +0000

    Write rendered template to tmp file for execution
    [#110987822]
    
    Signed-off-by: Jatin Naik <jnaik@pivotal.io>

--- spec/templates/rabbitmq_server_plugins_spec.rb
+++ spec/templates/rabbitmq_server_plugins_spec.rb
@@ -1,30 +1,22 @@
 require 'spec_helper'
 require 'bosh/template/renderer'
+require 'tempfile'
 
 describe 'Enabling plugins' do
   let(:manifest){ YAML.load_file('spec/support/manifest/test.yml')}
 
   context "when there is a plugin specified in the manifest but missing on server" do
     it "should not enable the specified plugin, but give a warning" do
-      renderer = Bosh::Template::Renderer.new({context: manifest.to_json})
-
-      rendered_template = renderer.render('jobs/rabbitmq-server/templates/plugins.sh.erb')
       plugins_list=%{rabbitmq_management
 rabbitmq_mqtt
       }
-
-      output=`RABBITMQ_PLUGINS=spec/templates/rabbitmq-plugins-stub.sh STUBBED_PLUGINS_LIST="#{plugins_list}" #{rendered_template}`
-
-
+      output = render_and_execute_template(plugins_list)
       expect(output).to eq("Ignoring unrecognised plugin: rabbitmq_stomp\n")
     end
   end
 
   context "when there is a plugin specified in the manifest but missing on server and is displayed as a warning" do
     it "should not enable the specified plugin, but give a warning" do
-      renderer = Bosh::Template::Renderer.new({context: manifest.to_json})
-
-      rendered_template = renderer.render('jobs/rabbitmq-server/templates/plugins.sh.erb')
       plugins_list=%{
 WARNING - plugins currently enabled but missing: [rabbitmq_stomp]
 
@@ -32,9 +24,7 @@ rabbitmq_management
 rabbitmq_mqtt
       }
 
-      output=`RABBITMQ_PLUGINS=spec/templates/rabbitmq-plugins-stub.sh STUBBED_PLUGINS_LIST="#{plugins_list}" #{rendered_template}`
-
-
+      output = render_and_execute_template(plugins_list)
       expect(output).to eq("Ignoring unrecognised plugin: rabbitmq_stomp\n")
     end
   end
@@ -42,17 +32,28 @@ rabbitmq_mqtt
 
   context "when there is a plugin specified in the manifest and present on the server" do
     it "should enable the specified plugin, but not give a warning" do
-      renderer = Bosh::Template::Renderer.new({context: manifest.to_json})
-
-      rendered_template = renderer.render('jobs/rabbitmq-server/templates/plugins.sh.erb')
       plugins_list=%{rabbitmq_management
 rabbitmq_mqtt
 rabbitmq_stomp
       }
 
-      output=`RABBITMQ_PLUGINS=spec/templates/rabbitmq-plugins-stub.sh STUBBED_PLUGINS_LIST="#{plugins_list}" #{rendered_template}`
-
+      output = render_and_execute_template(plugins_list)
       expect(output).to be_empty
     end
   end
+end
+
+
+def render_and_execute_template(plugins_list)
+  renderer = Bosh::Template::Renderer.new({context: manifest.to_json})
+  rendered_template = renderer.render('jobs/rabbitmq-server/templates/plugins.sh.erb')
+
+  command_file = Tempfile.new('plugins')
+  command_file.write(rendered_template)
+  command_file.close
+  File.chmod(0744, command_file.path)
+
+  output = `RABBITMQ_PLUGINS=spec/templates/rabbitmq-plugins-stub.sh STUBBED_PLUGINS_LIST="#{plugins_list}" #{command_file.path}`
+  command_file.unlink
+  output
 end
