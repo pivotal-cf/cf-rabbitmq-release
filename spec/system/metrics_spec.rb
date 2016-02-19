9925390a6d77deab8c8375f6eaddc07ce7e0eb93
Svett Ralchev <sralchev@pivotal.io>
2016-02-19 14:28:49 +0000

    Add RabbitMQ HAPorxy heartbeat metrics
    [#112422247]
    
    Signed-off-by: William Martin <wmartin@pivotal.io>

--- spec/system/metrics_spec.rb
+++ spec/system/metrics_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
+require 'date'
 
 describe 'metrics', :skip_metrics => true do
 
   before(:all) do
     @number_of_nodes = 1
     @outFile = Tempfile.new('smetrics')
+
     @pid = spawn(
       {
         'DOPPLER_ADDR' => doppler_address,
@@ -20,18 +22,28 @@ describe 'metrics', :skip_metrics => true do
     @outFile.unlink
   end
 
-  describe 'rabbitmq example metrics' do
-    ["/rabbitmq/example/heartbeat"
-    ].each do |metric_name|
-      it "contains #{metric_name} metric for rabbitmq haproxy nodes" do
-        @number_of_nodes.times do |idx|
-          assert_metric(metric_name, 'haproxy_z1', idx)
-        end
+  describe 'rabbitmq haproxy metrics' do
+    it "contains haproxy_z1 metric for rabbitmq haproxy nodes" do
+      assert_metric('/p-rabbitmq/haproxy/heartbeat', 'haproxy_z1', 0, /value:1 unit:"boolean"/)
+    end
+
+    context "when haproxy_z1 is not running" do
+      before(:all) do
+        @ha_host = bosh_director.ips_for_job('haproxy_z1', environment.bosh_manifest.deployment_name)[0]
+        ssh_gateway.execute_on(@ha_host, '/var/vcap/bosh/bin/monit stop rabbitmq-haproxy', :root => true)
+      end
+
+      after(:all) do
+        ssh_gateway.execute_on(@ha_host, '/var/vcap/bosh/bin/monit start rabbitmq-haproxy', :root => true)
+      end
+
+      it "contains haproxy_z1 metrics for rabbitmq haproxy nodes" do
+        assert_metric('/p-rabbitmq/haproxy/heartbeat', 'haproxy_z1', 0, /value:0 unit:"boolean"/)
       end
     end
   end
 
-  def assert_metric(metric_name, job_name, job_index)
+  def assert_metric(metric_name, job_name, job_index, *regex_patterns)
     metric = find_metric(metric_name, job_name, job_index)
 
     expect(metric).to match(/value:\d/)
@@ -41,15 +53,28 @@ describe 'metrics', :skip_metrics => true do
     expect(metric).to match(/timestamp:\d/)
     expect(metric).to match(/index:"\d"/)
     expect(metric).to match(/ip:"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"/)
+
+    regex_patterns.each do |expr|
+      expect(metric).to match(expr)
+    end
   end
 
   def find_metric(metric_name, job_name, job_index)
+    start_time = DateTime.now
+
     31.times do
       File.open(firehose_out_file, "r") do |file|
         regex = /(?=.*name:"#{metric_name}")(?=.*job:"#{job_name}")(?=.*index:"#{job_index}")/
         matches = file.readlines.grep(regex)
-        if matches.size > 0
-          return matches[0]
+
+        metrics = matches.select do |entry|
+          timestamp = entry.match(/timestamp:\d+/)[0].delete('timestamp:')
+          time = Time.at(timestamp.to_i/1e9).to_datetime
+          time >= start_time
+        end
+
+        if metrics.size > 0
+          return metrics[0]
         end
       end
       sleep 1
