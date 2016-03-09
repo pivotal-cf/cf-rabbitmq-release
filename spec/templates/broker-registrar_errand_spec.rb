3fc12902c185fd185fd210265e036ffc9a8ec923
Houssem El Fekih <hfekih@pivotal.io>
2016-03-09 12:12:00 +0000

    Add skip ssl validation property and make use of it
    * also add template test in spec
    [#114700967]
    
    Signed-off-by: Oleksandr Slynko <oslynko@pivotal.io>

---
+++ spec/templates/broker-registrar_errand_spec.rb
@@ -0,0 +1,43 @@
+require 'bosh/template/renderer'
+require 'yaml'
+
+RSpec.describe 'broker registration errand template' do
+	let(:renderer) {
+		Bosh::Template::Renderer.new({context: manifest.to_json})
+	}
+
+	let(:output) { renderer.render('jobs/broker-registrar/templates/errand.sh.erb') }
+
+	context 'when ssl validation is not skipped' do
+		let(:manifest){ emulate_bosh_director_merge ({'cf' => { 'skip-ssl-validation' => false }}) }
+
+		it 'skips ssl validation' do
+			expect(output).not_to include '--skip-ssl-validation'
+			expect(output).to include 'cf api'
+		end
+	end
+
+	context 'when ssl validation is skipped' do
+		let(:manifest){ emulate_bosh_director_merge ({'cf'=> {'skip-ssl-validation' => true }}) }
+
+		it 'skips ssl validation' do
+			expect(output).to include 'cf api --skip-ssl-validation'
+		end
+	end
+end
+
+
+include Bosh::Template::PropertyHelper
+
+# Trying to emulate bosh director Bosh::Director::DeploymentPlan::Job#extract_template_properties
+def emulate_bosh_director_merge(manifest_properties)
+	job_spec = YAML.load_file('jobs/broker-registrar/spec')
+	spec_properties = job_spec["properties"]
+
+	effective_properties = {}
+	spec_properties.each_pair do |name, definition|
+		copy_property(effective_properties, manifest_properties, name, definition["default"] || '')
+	end
+
+	{"properties" => effective_properties}
+end
