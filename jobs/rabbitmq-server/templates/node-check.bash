54c0276524573c709558637e38fcb89ab07dfd46
Gareth Smith <gsmith@pivotal.io>
2016-07-14 15:13:06 +0100

    Check that clusterer app is running part of node checks
    [#124356161]
    
    Signed-off-by: Gerhard Lazu <glazu@pivotal.io>

--- jobs/rabbitmq-server/templates/node-check.bash
+++ jobs/rabbitmq-server/templates/node-check.bash
@@ -9,6 +9,7 @@ LOG_DIR=/var/vcap/sys/log/rabbitmq-server
 
 main() {
   pid_file_contains_rabbitmq_erlang_vm_pid
+  clusterer_app_is_running
 }
 
 pid_file_contains_rabbitmq_erlang_vm_pid() {
@@ -18,6 +19,14 @@ pid_file_contains_rabbitmq_erlang_vm_pid() {
 
   [[ "$tracked_pid" = "$rabbitmq_erlang_vm_pid" ]] ||
   fail "Expected PID file to contain '$rabbitmq_erlang_vm_pid' but it contained '$tracked_pid'"
+}
+
+clusterer_app_is_running() {
+  local clusterer_app_info
+  clusterer_app_info="$(rabbitmqctl environment | grep -A 1 "{rabbitmq_clusterer")"
+
+  [[ "$clusterer_app_info" == *'{config,"/var/vcap/store/rabbitmq/etc/rabbitmq/cluster.config"}'* ]] ||
+  fail "RabbitMQ Clusterer app not running with correct configuration"
 }
 
 fail() {
