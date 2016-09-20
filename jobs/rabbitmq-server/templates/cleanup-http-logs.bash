32521decf8f853f733c957cd57961716ec26b367
Svett Ralchev <sralchev@pivotal.io>
2016-09-20 12:07:34 +0100

    Delete all old access logs
    [finishes #128487941]

--- jobs/rabbitmq-server/templates/cleanup-http-logs.bash
+++ jobs/rabbitmq-server/templates/cleanup-http-logs.bash
@@ -1,4 +1,18 @@
 #!/bin/bash
 
 source /var/vcap/jobs/rabbitmq-server/lib/delete_old_files.bash
-delete_old_files /var/vcap/sys/log/rabbitmq-server/management-ui
+
+main() {
+  delete_all_legacy_http_log_files
+  delete_http_log_files
+}
+
+delete_all_legacy_http_log_files() {
+  rm -f /var/vcap/sys/log/rabbitmq-server/access.log*
+}
+
+delete_http_log_files() {
+  delete_old_files /var/vcap/sys/log/rabbitmq-server/management-ui
+}
+
+main
