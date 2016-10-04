df2a6dcae92ae35414d422eb08c58aa133ab8663
Pivotal <pcf-rabbitmq@pivotal.io>
2016-10-04 14:09:48 +0100

    Revert "Delete all old access logs"
    We do not need this logic because the logs are stored on ephemeral disk
    
    [#131160871]
    
    This reverts commit e101559f1fec07e2db26b2dfdee1f56c8607bf11.

--- jobs/rabbitmq-server/templates/cleanup-http-logs.bash
+++ jobs/rabbitmq-server/templates/cleanup-http-logs.bash
@@ -1,18 +1,4 @@
 #!/bin/bash
 
 source /var/vcap/jobs/rabbitmq-server/lib/delete_old_files.bash
-
-main() {
-  delete_all_legacy_http_log_files
-  delete_http_log_files
-}
-
-delete_all_legacy_http_log_files() {
-  rm -f /var/vcap/sys/log/rabbitmq-server/access.log*
-}
-
-delete_http_log_files() {
-  delete_old_files /var/vcap/sys/log/rabbitmq-server/management-ui
-}
-
-main
+delete_old_files /var/vcap/sys/log/rabbitmq-server/management-ui
