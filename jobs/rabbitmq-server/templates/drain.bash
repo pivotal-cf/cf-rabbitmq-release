fdb67303d124cfdc21ca4a717f27749e0ee61972
Gareth Smith <gsmith@pivotal.io>
2016-10-05 10:50:33 +0100

    Remove Erlang PID file **after** we ensure RabbitMQ is stopped
    [#128761867]
    
    Signed-off-by: Gerhard Lazu <glazu@pivotal.io>

--- jobs/rabbitmq-server/templates/drain.bash
+++ jobs/rabbitmq-server/templates/drain.bash
@@ -13,8 +13,8 @@ SHUTDOWN_ERR_LOG="${LOG_DIR}"/shutdown_stderr.log
 main() {
   log "Attempting to stop RabbitMQ instance..."
   rabbitmqctl stop "$ERLANG_PID_FILE" 1>> "$SHUTDOWN_LOG" 2>> "$SHUTDOWN_ERR_LOG"
-  rm -f "$ERLANG_PID_FILE"
   # ensure RabbitMQ is stopped
+  rm -f "$ERLANG_PID_FILE"
   log "RabbitMQ instance stopped successfully"
   echo "0"
 }
