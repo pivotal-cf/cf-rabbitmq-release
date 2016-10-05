a544273a31ab5f90246f165076c7f4d34a45b5e2
Gareth Smith <gsmith@pivotal.io>
2016-10-05 10:50:11 +0100

    Log when attempting to shutdown RabbitMQ instance
    [#128761867]
    
    Signed-off-by: Gerhard Lazu <glazu@pivotal.io>

--- jobs/rabbitmq-server/templates/drain.bash
+++ jobs/rabbitmq-server/templates/drain.bash
@@ -11,11 +11,16 @@ SHUTDOWN_LOG="${LOG_DIR}"/shutdown_stdout.log
 SHUTDOWN_ERR_LOG="${LOG_DIR}"/shutdown_stderr.log
 
 main() {
+  log "Attempting to stop RabbitMQ instance..."
   rabbitmqctl stop "$ERLANG_PID_FILE" 1>> "$SHUTDOWN_LOG" 2>> "$SHUTDOWN_ERR_LOG"
   rm -f "$ERLANG_PID_FILE"
   # ensure RabbitMQ is stopped
-  echo "RabbitMQ stopped successfully" 1>> "$SHUTDOWN_LOG"
+  log "RabbitMQ instance stopped successfully"
   echo "0"
+}
+
+log() {
+  echo "$*" 1>> "$SHUTDOWN_LOG"
 }
 
 main
