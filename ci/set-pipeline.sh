d9cb453f5c9f2191395beb90c6aa14c5dc0ecb8d
Gareth Smith <gsmith@pivotal.io>
2016-10-06 15:52:05 +0100

    Create a set pipeline script
    Signed-off-by: Scott Muc <smuc@pivotal.io>

---
+++ ci/set-pipeline.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eu
+
+if ! which lpass > /dev/null 2>&1
+then
+  echo "must have lastpass CLI installed" >&2
+  exit 1
+fi
+
+ssh_key=$(lpass show "Shared-London Services"/london-ci/git-ssh-key --notes)
+
+fly -t london set-pipeline \
+    --pipeline rabbitmq-upgrade-preparation \
+    --config pipeline.yml \
+    --var git-private-key="${ssh_key}"
