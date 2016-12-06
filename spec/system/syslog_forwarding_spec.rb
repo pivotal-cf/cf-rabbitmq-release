3d516eabb88bb2f0f210e967cbceace4a81cf8f6
Scott Muc <smuc@pivotal.io>
2016-12-06 14:44:45 +0000

    Update rsyslog contant to reflect instance id
    - updated format from
       [job=rmq index=0]
      to
       [job=rmq index=0 id=1dfee927-8f23-41fe-bab3-4e28b4c2d110]
    - update tests to reflect new structure provide better feedback with
      regards to the search string that it is using to find the logs.
    - copying over 00-syslog_forwarder.conf instead of
      20-syslog_forwarder.conf as this eliminates the confusing duplication
      of the logfile config and template.
    
    [#135603263]
    
    Signed-off-by: Scott Muc <smuc@pivotal.io>
    Signed-off-by: Carlo Colombo <ccolombo@pivotal.io>

--- spec/system/syslog_forwarding_spec.rb
+++ spec/system/syslog_forwarding_spec.rb
@@ -15,40 +15,44 @@ DEPLOYMENT_INSTANCES = JSON.parse(
   )
 ).map { |i| OpenStruct.new(i) }
 
-RSpec.describe "Syslog forwarding" do
-  def logs_for_host(host)
+def host_search_string(host)
+  "[job=#{host.job} index=#{host.index} id=#{host.id}]"
+end
 
-    REMOTE_LOG_DESTINATION.query(
-      "#{DEPLOYMENT_NAME}/#{host.job}/#{host.id}", group_id: PAPERTRAIL_GROUP_ID
-    ).search.events
+RSpec.describe "Syslog forwarding" do
+  def search_for_events(search_string)
+    REMOTE_LOG_DESTINATION.query(search_string, group_id: PAPERTRAIL_GROUP_ID).search.events
   end
 
   describe "rmq_server hosts" do
-    let(:rmq_server_hosts) { DEPLOYMENT_INSTANCES.select { |i| i.job == "rmq" } }
+    rmq_server_hosts = DEPLOYMENT_INSTANCES.select { |i| i.job == "rmq" }
 
-    it "forwards rmq_server hosts logs" do
-      rmq_server_hosts.each do |rmq_server_host|
-        expect(logs_for_host(rmq_server_host).size).to be > 0
+    rmq_server_hosts.each do |rmq_server_host|
+      search_string = host_search_string(rmq_server_host)
+      it "forwards rmq_server hosts logs (#{search_string})" do
+        expect(search_for_events(search_string).size).to be > 0
       end
     end
   end
 
   describe "rmq_broker host" do
-    let(:rmq_broker_hosts) { DEPLOYMENT_INSTANCES.select { |i| i.job == "rmq-broker" } }
+    rmq_broker_hosts = DEPLOYMENT_INSTANCES.select { |i| i.job == "rmq-broker" }
 
-    it "forwards rmq_broker hosts logs" do
-      rmq_broker_hosts.each do |rmq_broker_host|
-        expect(logs_for_host(rmq_broker_host).size).to be > 0
+    rmq_broker_hosts.each do |rmq_broker_host|
+      search_string = host_search_string(rmq_broker_host)
+      it "forwards rmq_broker hosts logs (#{search_string})" do
+        expect(search_for_events(search_string).size).to be > 0
       end
     end
   end
 
   describe "rmq_haproxy host" do
-    let(:rmq_haproxy_hosts) { DEPLOYMENT_INSTANCES.select { |i| i.job == "haproxy" } }
+    rmq_haproxy_hosts = DEPLOYMENT_INSTANCES.select { |i| i.job == "haproxy" }
 
-    it "forwards rmq_haproxy hosts logs" do
-      rmq_haproxy_hosts.each do |rmq_haproxy_host|
-        expect(logs_for_host(rmq_haproxy_host).size).to be > 0
+    rmq_haproxy_hosts.each do |rmq_haproxy_host|
+      search_string = host_search_string(rmq_haproxy_host)
+      it "forwards rmq_haproxy hosts logs (#{search_string})" do
+        expect(search_for_events(search_string).size).to be > 0
       end
     end
   end
