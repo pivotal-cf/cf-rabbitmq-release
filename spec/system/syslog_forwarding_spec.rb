fbb6b6e232b632de353301f7e96544983f999378
Diego Lemos <dlresende@pivotal.io>
2016-12-19 17:59:06 +0000

    Increase seach scope on syslog tests
    **Problem**: Papertrail API establishes by default a [5 seconds limit]
    (http://help.papertrailapp.com/kb/how-it-works/http-api/#api-rate-limits)
    to perform event searches. In some cases, the search lasts for more than that
    limit making syslog tests fail.
    
    **Solution**: start using a `min_time` parameter in the papertrail test requests
    so that we can search using a time period based on log timestampts
    instead of using query time limits.
    
    [#136341747]
    
    Signed-off-by: Zhou Yu <zyu@pivotal.io>
    Signed-off-by: Diego Lemos <dlresende@pivotal.io>

--- spec/system/syslog_forwarding_spec.rb
+++ spec/system/syslog_forwarding_spec.rb
@@ -19,9 +19,18 @@ def host_search_string(host)
   "[job=#{host.job} index=#{host.index} id=#{host.id}]"
 end
 
+def one_day_ago
+  Time.now - (24 * 60 * 60)
+end
+
 RSpec.describe "Syslog forwarding" do
   def search_for_events(search_string)
-    REMOTE_LOG_DESTINATION.query(search_string, group_id: PAPERTRAIL_GROUP_ID).search.events
+    options = { :group_id => PAPERTRAIL_GROUP_ID, :min_time => one_day_ago }
+    events = []
+    REMOTE_LOG_DESTINATION.each_event(search_string, options) do |event|
+      events.push(event)
+    end
+    events
   end
 
   describe "rmq_server hosts" do
