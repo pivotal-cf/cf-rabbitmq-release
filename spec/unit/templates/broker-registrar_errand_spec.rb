57ae38a44e30cd2b3bb753ca90ad8e81f132f31c
Gareth Smith <gsmith@pivotal.io>
2016-12-28 10:51:32 +0000

    Fix typo in wording of broker-registrar unit test
    The previous (typoed) wording said that when the manifest asks us to
    perform SSL validation, we should skip it. This was the opposite of what
    we actually wanted.

--- spec/unit/templates/broker-registrar_errand_spec.rb
+++ spec/unit/templates/broker-registrar_errand_spec.rb
@@ -3,23 +3,23 @@ RSpec.describe 'broker registration errand template', template: true do
 		compiled_template('broker-registrar', 'errand.sh', manifest_properties)
 	}
 
-	context 'when ssl validation is not skipped' do
+	context 'when the manifest says to perform ssl validation' do
 		let(:manifest_properties) do
 			{'cf' => { 'skip_ssl_validation' => false }}
 		end
 
-		it 'skips ssl validation' do
+		it 'performs ssl validation in errand.sh' do
 			expect(output).not_to include '--skip-ssl-validation'
 			expect(output).to include 'cf api'
 		end
 	end
 
-	context 'when ssl validation is skipped' do
+	context 'when the manifest says to skip ssl validation' do
 		let(:manifest_properties) do
 			{'cf' => { 'skip_ssl_validation' => true }}
 		end
 
-		it 'skips ssl validation' do
+		it 'skips ssl validation in errand.sh' do
 			expect(output).to include 'cf api --skip-ssl-validation'
 		end
 	end
