b57f5e4dea5dcd0a84561d566da99e08ceceb375
Gerhard Lazu <gerhard@rabbitmq.com>
2017-01-16 16:40:16 +0000

    Optimise prepare_for_upgrade
    Do not run on the current node since rabbitmq-server should already be
    stopped. If it isn't, it means that the shutdown wasn't performed
    properly (i.e. bosh stop)
    
    If this is a first deploy, there is nothing to upgrade, so skip
    prepare_for_upgrade altogether.
    
    Signed-off-by: Jean-Sébastien Pedron <jean-sebastien@rabbitmq.com>

--- jobs/rabbitmq-server/templates/prepare-for-upgrade.bash
+++ jobs/rabbitmq-server/templates/prepare-for-upgrade.bash
@@ -15,7 +15,7 @@ STDERR_LOG="${LOG_DIR}"/pre-start.stderr.log
 main() {
   run_script "${JOB_DIR}/bin/setup.sh"
   run_script "${JOB_DIR}/bin/plugins.sh"
-  prepare_for_upgrade
+  is_first_deploy || prepare_for_upgrade
 }
 
 run_script() {
@@ -41,11 +41,22 @@ run_script() {
     esac
 }
 
+is_first_deploy() {
+  ! [ -e /var/vcap/store/rabbitmq/mnesia ]
+}
+
 prepare_for_upgrade () {
   echo "Preparing RabbitMQ for potential upgrade"
-  local remote_nodes
+  local remote_nodes local_node
   remote_nodes=($(cat /var/vcap/data/upgrade_preparation_nodes))
+  # shellcheck disable=SC1091
+  local_node=$(. /var/vcap/store/rabbitmq/etc/rabbitmq/rabbitmq-env.conf; echo "$NODENAME")
+
   for remote_node in "${remote_nodes[@]}"; do
+    # since the current node is already down,
+    # the rabbitmq-upgrade-preparation will timeout
+    # skip over the current node
+    [ "$remote_node" != "$local_node" ] || continue
     /var/vcap/packages/rabbitmq-upgrade-preparation/bin/rabbitmq-upgrade-preparation \
       -rabbitmqctl-path "${CONTROL}" \
       -node "$remote_node" \
