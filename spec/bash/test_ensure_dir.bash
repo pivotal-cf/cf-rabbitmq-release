8aade5eb947c709413ded837f4bc71900276bd6e
Steve Freeman <sfreeman@pivotal.io>
2017-03-16 17:51:16 +0000

    Do not follow symbolic links
    (CC not present)
    
    [#141709951]
    
    Signed-off-by: Carlo Colombo <ccolombo@pivotal.io>

--- spec/bash/test_ensure_dir.bash
+++ spec/bash/test_ensure_dir.bash
@@ -12,11 +12,17 @@ source spec/bash/test_helpers
 local testuser="testuser-$(date +%s)"
 sudo adduser --disabled-password --no-create-home --gecos "" $testuser
 
+pathstat() {
+  local path=${1:?path to check}
+
+  sudo stat "$path" -c '%A %U:%G'
+}
+
 check_path() {
     local path=${1:?path to check}
     local mode=${2:?mode to match}
 
-    expect_to_equal "$(sudo stat $path -c '%A %U:%G')" "$mode $testuser:$testuser"
+    expect_to_equal "$(pathstat $path)" "$mode $testuser:$testuser"
 }
 
 T_creates_missing_directory_and_sets_permissions() {
@@ -28,7 +34,7 @@ T_creates_missing_directory_and_sets_permissions() {
   check_path "$destination_dir" "drwxr-x---"
 }
 
-T_recursively_chown_all_the_subtree_under_target_directory() {
+T_assign_ownership_and_permissions_for_an_existing_directory() {
   local topdir=$(mktemp -d)
 
   local destination_dir="${topdir}/one/two"
@@ -44,4 +50,23 @@ T_recursively_chown_all_the_subtree_under_target_directory() {
   check_path "$nested_dir" "drwxrwx---"
   check_path "$nested_dir/a-file" "-rw-rw----"
   check_path "$nested_dir/an-executable-file" "-rwxrwx---"
+}
+
+T_does_not_follow_symbolic_links() {
+  local topdir=$(mktemp -d)
+
+  local linked_to="$topdir/linked_to"
+  touch ${linked_to}
+  chmod a+rw ${linked_to}
+  local original_stat="$(pathstat $linked_to)"
+
+  local destination_dir="${topdir}/one/two"
+  local linked_from="$destination_dir/linked_from"
+  mkdir -p "$destination_dir"
+  ln -s ${linked_to} ${linked_from}
+
+  sudo ./spec/bash/ensure_dir_wrapper.bash "$destination_dir" "$testuser:$testuser"
+
+  check_path "$destination_dir" "drwxr-x---"
+  expect_to_equal "$(pathstat $linked_to)" "$original_stat"
 }
