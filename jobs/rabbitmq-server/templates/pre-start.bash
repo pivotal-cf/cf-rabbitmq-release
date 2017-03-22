d6ca6077231ecaae2c642755021c49e58f5a387f
Gareth Smith <gsmith@pivotal.io>
2017-03-22 10:53:17 +0000

    Use ensure_dir_with_permissions instead of old ensure_dir
    [#142125509]
    
    Signed-off-by: Carlo Colombo <ccolombo@pivotal.io>

--- jobs/rabbitmq-server/templates/pre-start.bash
+++ jobs/rabbitmq-server/templates/pre-start.bash
@@ -15,14 +15,16 @@ SHUTDOWN_LOG="${INIT_LOG_DIR}"/shutdown_stdout.log
 SHUTDOWN_ERR_LOG="${INIT_LOG_DIR}"/shutdown_stderr.log
 USER=vcap
 
+source /var/vcap/packages/rabbitmq-common/ensure_dir_with_permissions
+
 main() {
-  ensure_dir "${ROOT_LOG_DIR}"
-  ensure_dir "${INIT_LOG_DIR}"
-  ensure_dir "${HTTP_ACCESS_LOG_DIR}"
-  ensure_dir "$(dirname "${PID_FILE}")"
-  ensure_dir "${HOME_DIR}"
-  ensure_dir "${JOB_DIR}"
-  ensure_dir "${SERVICE_METRICS_DIR}"
+  ensure_dir_with_permissions "${ROOT_LOG_DIR}"
+  ensure_dir_with_permissions "${INIT_LOG_DIR}"
+  ensure_dir_with_permissions "${HTTP_ACCESS_LOG_DIR}"
+  ensure_dir_with_permissions "$(dirname "${PID_FILE}")"
+  ensure_dir_with_permissions "${HOME_DIR}"
+  ensure_dir_with_permissions "${JOB_DIR}"
+  ensure_dir_with_permissions "${SERVICE_METRICS_DIR}"
   ensure_log_files
   ensure_http_log_cleanup_cron_job
   ${JOB_DIR}/bin/ensure-rabbitmq-statsdb-restart-cron
@@ -36,13 +38,6 @@ main() {
 
   # restart rsyslog to use the latest configuration
   /usr/sbin/service rsyslog restart
-}
-
-ensure_dir() {
-    _dir=$1
-    mkdir -p "${_dir}"
-    chown -LR "${USER}":"${USER}" "${_dir}"
-    chmod 750 "${_dir}"
 }
 
 ensure_log_files() {
