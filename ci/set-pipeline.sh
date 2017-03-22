be0dcd7d32426153f850c0adff2edbb1bd541b9d
Alberto Leal <aleal@pivotal.io>
2017-03-22 17:03:13 +0000

    Update concourse config
    Signed-off-by: Zhou Yu <zyu@pivotal.io>

--- ci/set-pipeline.sh
+++ ci/set-pipeline.sh
@@ -1,14 +1,4 @@
-#!/bin/bash -e
+#!/bin/bash
 
-if ! which lpass > /dev/null 2>&1
-then
-  echo "must have lastpass CLI installed" >&2
-  exit 1
-fi
-
-ssh_key=$(lpass show "Shared-London Services"/london-ci/git-ssh-key --notes)
-
-fly -t london set-pipeline \
-    --pipeline rabbitmq-upgrade-preparation \
-    --config pipeline.yml \
-    --var git-private-key="${ssh_key}"
+fly -t zumba set-pipeline --pipeline rabbitmq-cluster-migration-tool \
+  --config pipeline.yml \
