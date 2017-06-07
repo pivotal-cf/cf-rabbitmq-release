9af74f7e87e1094425c72d75d230e1ca12140abe
Diego Lemos <dlresende@pivotal.io>
2017-06-07 19:10:48 +0100

    Add toggle to activate rabbitmq_clusterer plugin
    In order to activate the clusterer plugin you need to set the
    `rabbitmq-server.use_native_clustering_formation = true`. Right now,
    this is the default behaviour, which means that if the property is not
    provided the native clustering formation will be used.
    
    However, there's currently a problem with this design: if the property
    is `rabbitmq-server.use_native_clustering_formation = true` and the
    `rabbitmq_clusterer` is present in `rabbitmq-server.plugins` list, the
    plugin will be enabled without the required configuration.
    
    In order to use the rabbitmq_clusterer plugin just set
    `rabbitmq-server.use_native_clustering_formation = false`.
    
    [#144893525]
    
    Signed-off-by: Abhishek Chander <achander@pivotal.io>

--- spec/system/clustering_spec.rb
+++ spec/system/clustering_spec.rb
@@ -34,54 +34,29 @@ RSpec.describe "RabbitMQ cluster configuration" do
     end
   end
 
-  # describe "uses clusterer plugin" do
-  #   before(:all) do
-  #     modify_and_deploy_manifest do |manifest|
-  #       manifest['properties']['rabbitmq-server']['use_native_clustering_formation'] = false
-  #     end
-  #   end
+  describe "uses clusterer plugin" do
+    before(:all) do
+      modify_and_deploy_manifest do |manifest|
+        manifest['properties']['rabbitmq-server']['use_native_clustering_formation'] = false
+      end
+    end
 
-  #   it "should have all the nodes running on the cluster" do
-  #     nodes = get_nodes(rmq_management_ui_url, rmq_username, rmq_password)
+    it "should have all the nodes running on the cluster" do
+      nodes = get_nodes(rmq_management_ui_url, rmq_username, rmq_password)
 
-  #     expect(nodes.size).to eql(nb_of_rmq_instances)
-  #     nodes.each { |node| expect(node['running']).to be true }
-  #   end
+      expect(nodes.size).to eql(nb_of_rmq_instances)
+      nodes.each { |node| expect(node['running']).to be true }
+    end
 
-  #   it "should have the clusterer plugin" do
-  #     nodes = get_nodes(rmq_management_ui_url, rmq_username, rmq_password)
-  #     nodes.each { |node| expect(node['enabled_plugins']).to include "rabbitmq_clusterer" }
-  #   end
+    it "should have the clusterer plugin" do
+      nodes = get_nodes(rmq_management_ui_url, rmq_username, rmq_password)
+      nodes.each { |node| expect(node['enabled_plugins']).to include "rabbitmq_clusterer" }
+    end
 
-  #   after(:all) do
-  #     bosh_director.deploy(environment.bosh_manifest.path)
-  #   end
-  # end
-
-#   describe "no clustering property provided," do
-
-#     before(:all) do
-#       modify_and_deploy_manifest do |manifest|
-#         manifest['properties']['rabbitmq-server'].delete('use_native_clustering_formation')
-#       end
-#     end
-
-#     it "should have all the nodes running on the cluster" do
-#       nodes = get_nodes(rmq_management_ui_url, rmq_username, rmq_password)
-
-#       expect(nodes.size).to eql(nb_of_rmq_instances)
-#       nodes.each { |node| expect(node['running']).to be true }
-#     end
-
-#     it "should have the clusterer plugin" do
-#       nodes = get_nodes(rmq_management_ui_url, rmq_username, rmq_password)
-#       nodes.each { |node| expect(node['enabled_plugins']).to include "rabbitmq_clusterer" }
-#     end
-
-#     after(:all) do
-#       bosh_director.deploy(environment.bosh_manifest.path)
-#     end
-#   end
+    after(:all) do
+      bosh_director.deploy(environment.bosh_manifest.path)
+    end
+  end
 end
 
 def get_nodes(management_url, username, password)
