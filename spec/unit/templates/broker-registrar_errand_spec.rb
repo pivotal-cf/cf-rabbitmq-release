fa7dae2550cb5951cfd770b73655ad9fc5da357c
Carlo Colombo <ccolombo@pivotal.io>
2017-07-20 15:58:40 +0100

    Remove broker-[re|de]gistrar integration tests
    * Usually those tests are quite expensive lasting for almost an hour and
    we can tests that in the unit level by checking if the errands execute
    the expected cf command. We rely on other tests in our pipeline to fail
    if there's any integration issue when executing `bosh run errand`.
    
    [#142052159]
    
    Signed-off-by: Alberto Leal <aleal@pivotal.io>

--- spec/unit/templates/broker-registrar_errand_spec.rb
+++ spec/unit/templates/broker-registrar_errand_spec.rb
@@ -1,27 +1,48 @@
-RSpec.describe 'broker registration errand template', template: true do
-	let(:output) {
-		compiled_template('broker-registrar', 'errand.sh', manifest_properties)
-	}
+RSpec.describe 'broker-registrar errand template', template: true do
+  let(:output) {
+    compiled_template('broker-registrar', 'errand.sh', manifest_properties)
+  }
 
-	context 'when the manifest says to perform ssl validation' do
-		let(:manifest_properties) do
-			{'cf' => { 'skip_ssl_validation' => false }}
-		end
+  context 'when the manifest says to perform ssl validation' do
+    let(:manifest_properties) do
+      {'cf' => { 'skip_ssl_validation' => false }}
+    end
 
-		it 'performs ssl validation in errand.sh' do
-			expect(output).not_to include '--skip-ssl-validation'
-			expect(output).to include 'cf api'
-		end
-	end
+    it 'performs ssl validation in errand.sh' do
+      expect(output).not_to include '--skip-ssl-validation'
+      expect(output).to include 'cf api'
+    end
+  end
 
-	context 'when the manifest says to skip ssl validation' do
-		let(:manifest_properties) do
-			{'cf' => { 'skip_ssl_validation' => true }}
-		end
+  context 'when the manifest says to skip ssl validation' do
+    let(:manifest_properties) do
+      {'cf' => { 'skip_ssl_validation' => true }}
+    end
 
-		it 'skips ssl validation in errand.sh' do
-			expect(output).to include 'cf api --skip-ssl-validation'
-		end
-	end
+    it 'skips ssl validation in errand.sh' do
+      expect(output).to include 'cf api --skip-ssl-validation'
+    end
+  end
+
+  context 'when the orgs propery is not set' do
+    let(:manifest_properties) do
+      {'broker-registrar' => { 'orgs' => [] }}
+    end
+
+    it 'enables access to the service broker for all orgs' do
+      expect(output).to include 'cf enable-service-access $SERVICE_NAME'
+    end
+  end
+
+  context 'when the orgs property is set' do
+    let(:manifest_properties) do
+      {'broker-registrar' => { 'orgs' => ['org1','org2'] }}
+    end
+
+    it 'enables access to the service broker for the specified orgs only' do
+      expect(output).to include 'cf enable-service-access $SERVICE_NAME -o org1'
+      expect(output).to include 'cf enable-service-access $SERVICE_NAME -o org2'
+    end
+  end
 end
 
