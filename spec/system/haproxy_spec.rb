d35c5a66f82679915b9f4be25919b10075c74b12
Ben Chapman <bchapman@pivotal.io>
2017-08-22 10:00:13 +0100

    Fix haproxy tests so we don't have to push a CF app
    Signed-off-by: Carlo Colombo <ccolombo@pivotal.io>

--- spec/system/haproxy_spec.rb
+++ spec/system/haproxy_spec.rb
@@ -9,17 +9,10 @@ require 'net/http'
 
 RSpec.describe "haproxy" do
 
-  let(:service_name) { environment.bosh_manifest.property('rabbitmq-broker.service.name') }
-
-  let(:service) do
-    Prof::MarketplaceService.new(
-      name: service_name,
-      plan: 'standard'
-    )
-  end
+  let(:management_uri) { 'https://' + environment.bosh_manifest.property('rabbitmq-broker.rabbitmq.management_domain') }
 
   [0, 1].each do |job_index|
-    context "when the job rmq/#{job_index} is down", :pushes_cf_app do
+    context "when the job rmq/#{job_index} is down" do
       before(:all) do
         `bosh -n stop rmq #{job_index} --force`
       end
@@ -28,18 +21,16 @@ RSpec.describe "haproxy" do
         `bosh -n start rmq #{job_index} --force`
       end
 
-      it "is still possible to read and write to a queue" do
-        cf.push_app_and_bind_with_service(test_app, service) do |app, _|
-          uri = URI("#{app.url}/services/rabbitmq/protocols/amqp091")
+      it 'I can still access the managment UI' do
+        uri = URI(management_uri)
 
-          http = Net::HTTP.new(uri.host, uri.port)
-          http.use_ssl = true
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          res = http.request_get(uri.request_uri)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        res = http.request_get(uri.request_uri)
 
-          expect(res.code).to eql("200")
-          expect(res.body).to include("amq.gen")
-        end
+        expect(res.code).to eql('200')
+        expect(res.body).to include('RabbitMQ Management')
       end
     end
   end
