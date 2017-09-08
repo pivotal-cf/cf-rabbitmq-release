56193e2822a60ba3e092c01324caca3051596cdf
Ben Chapman <bchapman@pivotal.io>
2017-09-08 11:06:14 +0100

    Remove rabbitmq-cluster-migration-tool
    [#150886904]
    
    Signed-off-by: Andrew Thorburn <athorburn@pivotal.io>

--- jobs/rabbitmq-server/templates/node_name_migration.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-#
-# node name migration variables
-PATH=$PATH:/var/vcap/packages/erlang/bin
-PATH=$PATH:/var/vcap/packages/rabbitmq-server/bin
-PATH=$PATH:/var/vcap/packages/rabbitmq-cluster-migration-tool/bin
-MIGRATION_DIR=/tmp/node_name_migration
-MIGRATION_TOOL=/var/vcap/packages/rabbitmq-cluster-migration-tool/bin/rabbitmq-cluster-migration-tool
