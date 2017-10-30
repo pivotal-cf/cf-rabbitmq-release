83173130cd5f50055b048aa724cf65bd504a0e96
Alberto Leal <albertonb@gmail.com>
2017-10-30 09:09:17 +0000

    Create unit tests for each method isolated
    [#136735635]

--- spec/bash/setup_test.bash
+++ spec/bash/setup_test.bash
@@ -12,249 +12,186 @@ export T_fail
 # shellcheck disable=SC1091
 . jobs/rabbitmq-server/templates/setup.bash
 
-before_each() {
+T_setup_environment() {
+  (
+    local env
+
     DIR="$(mktemp -d)"
     trap "rm -rf ${DIR}" EXIT
-}
-
-T_expects_to_create_an_env_file_in_given_dir_with_config_to_rabbitmq() {
-  (
-    before_each
-
-    main
-
-    expect_file_to_exist "${DIR}/env"
-    expect_to_equal "$(<$DIR/env.backup)" ""
-  )
-}
-
-T_expects_to_set_cluster_partition_handling() {
-  (
-    before_each
 
     CLUSTER_PARTITION_HANDLING="autoheal"
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbit cluster_partition_handling autoheal"
-
-  ) || $T_fail "Cluster partition handling not used to configure RabbitMQ"
-}
-
-T_expects_to_define_log_levels() {
-  (
-    before_each
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbit log_levels [{connection,info}]"
-
-  ) || $T_fail "Log levels not set properly"
-}
-
-T_expects_to_set_disk_free_limit_threshold() {
-  (
-    before_each
-
-
     DISK_ALARM_THRESHOLD="{mem_relative,0.4}"
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbit disk_free_limit {mem_relative,0.4}"
-
-  ) || $T_fail "Disk free limit threshold not set properly"
-}
-
-T_expects_not_to_halt_on_uprade_failure() {
-  (
-    before_each
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbit halt_on_upgrade_failure false"
-
-  ) || $T_fail "Halt on upgrade failure not set properly"
-}
-
-T_expects_subscription_ttl_to_be_set_for_mqtt() {
-  (
-    before_each
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbitmq_mqtt subscription_ttl 1800000"
-
-  ) || $T_fail "Subscription TTL not set for MQTT properly"
-}
-
-T_expects_management_http_log_dir_to_be_set() {
-  (
-    before_each
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbitmq_management http_log_dir \"${HTTP_ACCESS_LOG_DIR}\""
-
-  ) || $T_fail "Management HTTP logs not set properly"
-}
-
-T_expects_to_configure_rabbitmq_environment_variables() {
-  (
-    before_each
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" "RABBITMQ_MNESIA_DIR=/var/vcap/store/rabbitmq/mnesia/db"
-    expect_to_contain "$env" "RABBITMQ_PLUGINS_EXPAND_DIR=/var/vcap/store/rabbitmq/mnesia/db-plugins-expand"
-
-  ) || $T_fail "Environment variables not configured properly for RabbitMQ"
-}
-
-T_expects_to_create_a_backup_for_env_config_if_it_already_exists() {
-  (
-    before_each
-
-    CONF_ENV_FILE="$(mktemp)"
-    trap "rm -rf ${CONF_ENV_FILE}" EXIT
-    echo "a config content" > $CONF_ENV_FILE
-
-    expect_file_not_to_exist $DIR/env.backup
-    main
-    expect_file_to_exist $DIR/env.backup
-
-    expect_to_contain  "$(<$CONF_ENV_FILE)" "$(<$DIR/env.backup)"
-    expect_to_contain  "$(<$CONF_ENV_FILE)" "$(<$DIR/env)"
-
-  ) || $T_fail "Failed to create backup for env config file"
-}
-
-T_expects_to_configure_nodename() {
-  (
-    before_each
-
     SELF_NODE="my-node-name"
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" "NODENAME='my-node-name'"
-
-  ) || $T_fail "Environment does not have the correct NODENAME"
-}
-
-T_expects_to_configure_boot_module_to_be_rabbit() {
-  (
-    before_each
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" "RABBITMQ_BOOT_MODULE=rabbit"
-
-  ) || $T_fail "Environment does not have the correct NODENAME"
-}
-
-T_expects_to_configure_config_file_path() {
-  (
-    before_each
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" "CONFIG_FILE='"
-
-  ) || $T_fail "Environment does not have the correct CONFIG_FILE"
-}
-
-T_expects_to_configure_load_definitions_if_given() {
-  (
-    before_each
     LOAD_DEFINITIONS="my-definitions"
 
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbitmq_management load_definitions"
-
-  ) || $T_fail "Failed to load load definitions config"
-}
-
-T_expects_to_configure_ssl_listeners() {
-  (
-    before_each
-
     SSL_KEY="ssk-key"
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbit tcp_listeners []"
-    expect_to_contain "$env" " -rabbit ssl_listeners [5671]"
-    expect_to_contain "$env" "{verify,verify_none},"
-    expect_to_contain "$env" " -rabbitmq_management listener [{port,15672},{ssl,false}]"
-    expect_to_contain "$env" " -rabbitmq_mqtt ssl_listeners [8883]"
-    expect_to_contain "$env" " -rabbitmq_stomp ssl_listeners [61614]"
-
-  ) || $T_fail "Failed to configure SSL listeners"
-}
-
-T_expects_to_configure_ssl_options() {
-  (
-    before_each
-
-    SSL_KEY="ssk-key"
-    SSL_VERIFY=true
+    SSL_VERIFY=false
     SSL_VERIFICATION_DEPTH="5"
     SSL_FAIL_IF_NO_PEER_CERT=true
     SSL_SUPPORTED_TLS_VERSIONS="['tlsv1.2','tlsv1.1']"
-
-    main
-
-    env="$(<$DIR/env)"
-    expect_to_contain "$env" " -rabbit ssl_options [{cacertfile,"
-    expect_to_contain "$env" "{certfile,"
-    expect_to_contain "$env" "{keyfile,"
-    expect_to_contain "$env" "{verify,verify_peer},"
-    expect_to_contain "$env" "{depth,5},"
-    expect_to_contain "$env" "{fail_if_no_peer_cert,true},"
-    expect_to_contain "$env" "{versions,['tlsv1.2','tlsv1.1']}]"
-
-  ) || $T_fail "Failed to configure SSL options"
-}
-
-
-T_expects_to_create_erlang_cookie_file_with_right_permissions() {
-  (
-    before_each
 
     ERLANG_COOKIE="my-awesome-cookie"
     ERLANG_COOKIE_OWNER="${USER}"
 
     main
 
+    env="$(<$DIR/env)"
+    expect_file_to_exist "${DIR}/env"
+    expect_to_equal "$(<$DIR/env.backup)" ""
+
+    expect_to_contain "$env" " -rabbit cluster_partition_handling autoheal"
+    expect_to_contain "$env" " -rabbit log_levels [{connection,info}]"
+    expect_to_contain "$env" " -rabbit disk_free_limit {mem_relative,0.4}"
+    expect_to_contain "$env" " -rabbit halt_on_upgrade_failure false"
+    expect_to_contain "$env" " -rabbitmq_mqtt subscription_ttl 1800000"
+    expect_to_contain "$env" " -rabbitmq_management http_log_dir \"${HTTP_ACCESS_LOG_DIR}\""
+    expect_to_contain "$env" "RABBITMQ_MNESIA_DIR=/var/vcap/store/rabbitmq/mnesia/db"
+    expect_to_contain "$env" "RABBITMQ_PLUGINS_EXPAND_DIR=/var/vcap/store/rabbitmq/mnesia/db-plugins-expand"
+    expect_to_contain "$env" "NODENAME='my-node-name'"
+    expect_to_contain "$env" "RABBITMQ_BOOT_MODULE=rabbit"
+    expect_to_contain "$env" "CONFIG_FILE='"
+    expect_to_contain "$env" " -rabbitmq_management load_definitions"
+    expect_to_contain "$env" " -rabbit tcp_listeners []"
+    expect_to_contain "$env" " -rabbit ssl_listeners [5671]"
+    expect_to_contain "$env" "{verify,verify_none},"
+
+    # SSL
+    expect_to_contain "$env" " -rabbitmq_management listener [{port,15672},{ssl,false}]"
+    expect_to_contain "$env" " -rabbitmq_mqtt ssl_listeners [8883]"
+    expect_to_contain "$env" " -rabbitmq_stomp ssl_listeners [61614]"
+    expect_to_contain "$env" " -rabbit ssl_options [{cacertfile,"
+    expect_to_contain "$env" "{certfile,"
+    expect_to_contain "$env" "{keyfile,"
+    expect_to_contain "$env" "{verify,verify_none},"
+    expect_to_contain "$env" "{depth,5},"
+    expect_to_contain "$env" "{fail_if_no_peer_cert,true},"
+    expect_to_contain "$env" "{versions,['tlsv1.2','tlsv1.1']}]"
+
+    # ERLANG COOKIE
     erlang_cookie_path="${DIR}/.erlang.cookie"
     expect_file_to_exist $erlang_cookie_path
-
     erlang_cookie="$(<$erlang_cookie_path)"
     expect_to_contain "$erlang_cookie" "my-awesome-cookie"
-    # check_path "$erlang_cookie_path" "-r--------"
+
+  ) || $T_fail "Failed to configure environment"
+}
+
+
+T_create_cluster_args() {
+  (
+    local rabbitmq_nodes disk_alarm_threshold cluster_partition_handling http_access_log_dir cluster_args
+
+    rabbitmq_nodes="node-1,node-2"
+    disk_alarm_threshold="{mem_relative,0.4}"
+    cluster_partition_handling="autoheal"
+    http_access_log_dir="/path/to/http-access.log"
+
+    cluster_args="$(create_cluster_args $rabbitmq_nodes $disk_alarm_threshold $cluster_partition_handling, $http_access_log_dir)"
+
+    expect_to_contain "$cluster_args" "-rabbit cluster_nodes {[node-1,node-2],disc}"
+    expect_to_contain "$cluster_args" " -rabbit log_levels [{connection,info}]"
+    expect_to_contain "$cluster_args" " -rabbit disk_free_limit {mem_relative,0.4}"
+    expect_to_contain "$cluster_args" " -rabbit cluster_partition_handling autoheal"
+    expect_to_contain "$cluster_args" " -rabbit halt_on_upgrade_failure false"
+    expect_to_contain "$cluster_args" " -rabbitmq_mqtt subscription_ttl 1800000"
+    expect_to_contain "$cluster_args" " -rabbitmq_management http_log_dir \"/path/to/http-access.log\""
+
+  ) || $T_fail "Failed to create cluster args to pass to SERVER_START_ARGS"
+}
+
+T_format_server_start_args() {
+  (
+    local cluster_args server_start_args
+
+    cluster_args="my-cluster-args"
+
+    server_start_args="$(format_server_start_args $cluster_args)"
+    expect_to_equal "$server_start_args" "SERVER_START_ARGS='my-cluster-args'"
+
+  ) || $T_fail "Failed to formart SERVER_START_ARGS"
+}
+
+T_configure_load_definitions() {
+  (
+    local load_definitions script_dir load_definitions_file_path definitions
+
+    load_definitions="my-definitions"
+    script_dir="/path/to/script/dir"
+
+    definitions="$(configure_load_definitions $load_definitions $script_dir)"
+    expect_to_equal "$definitions" "-rabbitmq_management load_definitions \"/path/to/script/dir/../etc/definitions.json\""
+
+  ) || $T_fail "Failed to load definitions"
+}
+
+T_configure_tls_listeners() {
+  (
+    local ssl_key listeners
+
+    ssl_key="--this is my key--"
+
+    listeners="$(configure_tls_listeners \"$ssl_key\")"
+    expect_to_equal "$listeners" "-rabbit tcp_listeners [] -rabbit ssl_listeners [5671] -rabbitmq_management listener [{port,15672},{ssl,false}] -rabbitmq_mqtt ssl_listeners [8883] -rabbitmq_stomp ssl_listeners [61614]"
+
+  ) || $T_fail "Failed to configure TLS listeners"
+}
+
+T_configure_tls_options() {
+  (
+    local ssl_key ssl_verify ssl_verification_mode ssl_verification_depth script_dir ssl_fail_if_no_peer_cert ssl_supported_tls_versions ssl_options options
+
+    ssl_key="--this is my key--"
+    ssl_verify="true"
+    ssl_verification_depth="5"
+    ssl_fail_if_no_peer_cert=true
+    ssl_supported_tls_versions="['tlsv1.2','tlsv1.1']"
+    script_dir="/path/to/script/dir"
+
+    options="$(configure_tls_options "${ssl_key}" "${ssl_verify}" "${ssl_verification_depth}" "${ssl_fail_if_no_peer_cert}" "${ssl_supported_tls_versions}" "${script_dir}")"
+    expect_to_equal "$options" "\" -rabbit ssl_options [{cacertfile,\\\"${script_dir}/../etc/cacert.pem\\\"},{certfile,\\\"${script_dir}/../etc/cert.pem\\\"},{keyfile,\\\"${script_dir}/../etc/key.pem\\\"},{verify,verify_peer},{depth,$ssl_verification_depth},{fail_if_no_peer_cert,$ssl_fail_if_no_peer_cert},{versions,$ssl_supported_tls_versions}]\""
+
+  ) || $T_fail "Failed to configure TLS options"
+}
+
+T_create_config_file() {
+  (
+    local conf_env_file self_node dir nodename config script_dir prefix suffix server_start_args
+
+    conf_env_file=""
+    self_node="node-1"
+    dir="$(mktemp -d)"
+    script_dir="/path/to/script/dir"
+    server_start_args="SERVER_START_ARGS='this-is-my-config'"
+
+    trap "rm -rf ${dir}" EXIT
+
+    create_config_file "$conf_env_file" "$self_node" "$dir" "$script_dir" "$server_start_args"
+
+    expect_file_to_exist "${dir}/env"
+    expect_file_to_exist "${dir}/env.backup"
+
+    expect_to_contain "$(<$dir/env)" "CONFIG_FILE='/path/to/script/dir/../etc/rabbitmq'"
+    expect_to_contain "$(<$dir/env)" "NODENAME='node-1'"
+    expect_to_contain "$(<$dir/env)" "SERVER_START_ARGS='this-is-my-config'"
+    expect_to_contain "$(<$dir/env)" "RABBITMQ_MNESIA_DIR=/var/vcap/store/rabbitmq/mnesia/db"
+    expect_to_contain "$(<$dir/env)" "RABBITMQ_PLUGINS_EXPAND_DIR=/var/vcap/store/rabbitmq/mnesia/db-plugins-expand"
+
+  ) || $T_fail "Failed to create conf_env file"
+}
+
+T_create_erlang_cookie() {
+  (
+    #before we need to create vcap user
+    sudo adduser --disabled-password --gecos "" vcap
+
+    local erlang_cookie dir
+
+    erlang_cookie="this-is-my-cookie"
+    dir="$(mktemp -d)"
+    trap "rm -rf ${dir}" EXIT
+
+    create_erlang_cookie "$dir" "$erlang_cookie" "vcap"
+    expect_file_to_exist "${dir}/.erlang.cookie"
+    expect_to_equal "$(<$dir/.erlang.cookie)" "$erlang_cookie"
   )
 }
-
-pathstat() {
-  local path=${1:?path to check}
-
-  stat "$path" -c '%A %U:%G'
-}
-
-check_path() {
-    local path=${1:?path to check}
-    local mode=${2:?mode to match}
-
-    expect_to_equal "$(pathstat $path)" "$mode vcap:vcap"
-  }
