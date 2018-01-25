db78b148d77794679a236181fcde2e0bc908b555
Diego Lemos <dlresende@pivotal.io>
2018-01-25 14:26:13 +0000

    Test against Shell injection in users template
    [#139274045]

---
+++ spec/unit/templates/users_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+require 'bosh/template/renderer'
+
+RSpec.describe 'Users credentials file', template: true do
+  let(:users_config) {
+    compiled_template('rabbitmq-server', 'users', {
+      'rabbitmq-server' => {
+        'administrators' => {
+          'management' => {
+            'username' => 'my${SHELL}username',
+            'password' => 'my${SHELL}password'
+          },
+          'broker' => {
+            'username' => 'my${SHELL}username',
+            'password' => 'my${SHELL}password'
+          }
+        }
+      }
+    })
+  }
+
+  it 'does not allow Shell injection by escaping special characters' do
+    expect(users_config).to include('RMQ_OPERATOR_USERNAME="my\$\{SHELL\}username"')
+    expect(users_config).to include('RMQ_OPERATOR_PASSWORD="my\$\{SHELL\}password"')
+    expect(users_config).to include('RMQ_BROKER_PASSWORD="my\$\{SHELL\}password"')
+  end
+end
