e25da6b365860de5342bd0d32bdb35a14d74d5eb
David Ansari <dansari@pivotal.io>
2020-07-07 15:52:28 +0000

    Simplify getting the IP address
    see https://github.com/cloudfoundry/bosh/issues/1476#issuecomment-256170179
    
    This change assumes that no dynamic networks are used because
    https://bosh.io/docs/jobs/#properties-spec mentions "When dynamic
    networks are being used, spec.ip might not be available."
    
    [#173038128]
    
    Co-authored-by: Harsha Chaudhary <hchaudhary@pivotal.io>

--- spec/unit/templates/rabbitmq_config_vars_spec.rb
+++ spec/unit/templates/rabbitmq_config_vars_spec.rb
@@ -1,50 +1,44 @@
 require 'spec_helper'
-require 'bosh/template/renderer'
+require 'bosh/template/test'
 
 RSpec.describe 'Configuration', template: true do
-  let(:rendered_template) {
-    compiled_template('rabbitmq-server', 'rabbitmq-config-vars.bash', manifest_properties, links, network_properties)
-  }
-  let(:manifest_properties) do
+
+  let(:release) { Bosh::Template::Test::ReleaseDir.new(File.join(File.dirname(__FILE__), '../../..')) }
+  let(:job) { release.job('rabbitmq-server') }
+  let(:template) { job.template('lib/rabbitmq-config-vars.bash') }
+  let(:manifest) do
     {
       'rabbitmq-server' => {
         'cookie' => 'foobar1234'
       }
     }
   end
-  let(:links) do
-    {
-      'rabbitmq-server' => {
-        'instances' => [
-          { 'address' => '1.1.1.1' },
-          { 'address' => '2.2.2.2' }
-        ]
-      }
-    }
-  end
-  let(:network_properties) { { blah: { ip: '127.0.0.1', default: true }}}
+  let(:instance) { Bosh::Template::Test::InstanceSpec.new(ip: '1.1.1.1') }
+  let(:link) { Bosh::Template::Test::Link.new(name:'rabbitmq-server', instances:link_instances) }
 
-  describe "nodes" do
-    context "when there is only one rabbitmq-server instance" do
-      let(:links) do
-        {
-          'rabbitmq-server' => {
-            'instances' => [
-              { 'address' => '1.1.1.1' }
-            ]
-          }
-        }
-      end
+  let(:rendered_template) {
+    template.render(manifest, spec: instance, consumes: [link])
+  }
 
-      it "should contain only localhost in cluster" do
-        expect(rendered_template).to include("ERL_INETRC_HOSTS='{host, {127,0,0,1}, [\"f528764d624db129b32c21fbca0cb8d6\"]}.\n'")
+  describe 'nodes' do
+    context 'when there is only one rabbitmq-server instance' do
+      let(:link_instances) { [] }
+
+      it 'should contain only localhost in cluster' do
+        expect(rendered_template).to include("ERL_INETRC_HOSTS='{host, {1,1,1,1}, [\"e086aa137fa19f67d27b39d0eca18610\"]}.\n'")
       end
     end
 
     context 'when there are multiple rabbitmq-server instances' do
-      it "should contain all nodes in cluster" do
-        expect(rendered_template).to include("ERL_INETRC_HOSTS='{host, {1,1,1,1}, [\"e086aa137fa19f67d27b39d0eca18610\"]}.")
-        expect(rendered_template).to include("\n{host, {2,2,2,2}, [\"5b8656aafcb40bb58caf1d17ef8506a9\"]}.\n'")
+      let(:link_instances) do [
+        Bosh::Template::Test::InstanceSpec.new(address: '1.1.1.2'),
+        Bosh::Template::Test::InstanceSpec.new(address: '1.1.1.3')
+      ]
+      end
+
+      it 'should contain all nodes in cluster' do
+        expect(rendered_template).to include("ERL_INETRC_HOSTS='{host, {1,1,1,2}, [\"98660805cdee362a748327ad6032805b\"]}.")
+        expect(rendered_template).to include("\n{host, {1,1,1,3}, [\"82e630489bbfe8340627fb3fdad6134c\"]}.\n'")
       end
     end
   end
