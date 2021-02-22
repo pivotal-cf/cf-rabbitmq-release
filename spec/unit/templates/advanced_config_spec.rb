b0b8f1e035f7436680c742d6b5c33e67d04276d3
Connor Rogers <rconnor@vmware.com>
2021-02-22 17:10:14 +0000

    Move all OAuth config to conf.d directory
    As of https://github.com/rabbitmq/rabbitmq-server/pull/2833, it is
    possible to fully configure OAuth in the release through the
    Cuttlefish-style conf.d config files. This PR moves any conflicting
    advanced.config OAuth configuration to these files.
    
    A pre-requisite of this PR is
    https://github.com/pivotal-cf/cf-rabbitmq-release/pull/429 and the
    release of RabbitMQ 3.8.13.

--- spec/unit/templates/advanced_config_spec.rb
+++ spec/unit/templates/advanced_config_spec.rb
@@ -1,37 +1,11 @@
-RSpec.describe 'advanced.config file generation for oauth', template: true do
+RSpec.describe 'advanced.config file generation', template: true do
 	let(:output) do
     compiled_template('rabbitmq-server', 'config-files/advanced.config', manifest_properties).strip
 	end
 
-	context 'when oauth is not defined' do
-		let(:manifest_properties) { {} }
-
-		it 'defaults to []. valid erlang config' do
-			expect(output).to eq("[].")
-    end
-	end
-
-	context 'when oauth is disabled' do
+  context 'when custom advanced.config is provided' do
 		let(:manifest_properties) { {
 				'rabbitmq-server' => {
-          'oauth' => {
-            'enabled': false
-          }
-		    }
-      } 
-    }
-
-		it 'defaults to []. valid erlang config' do
-			expect(output).to eq("[].")
-    end
-	end
-
-  context 'when oauth is disabled and custom advanced.config is provided' do
-		let(:manifest_properties) { {
-				'rabbitmq-server' => {
-          'oauth' => {
-            'enabled': false
-          },
           'override_advanced_config': 'WwogIHtyYWJiaXQsIFsKICAgICAge3RjcF9saXN0ZW5lcnMsIFs1NjczXX0KICAgIF0KICB9Cl0u'
 		    }
       } 
@@ -47,7 +21,10 @@ RSpec.describe 'advanced.config file generation for oauth', template: true do
     end
 	end
 
-  context 'when oauth is enabled' do
+  # In previous releases, it was not possible to configure both override_advanced_config
+  # and OAuth config. Since the latter has been moved to the conf.d config files,
+  # only the override config should be set in this file now.
+  context 'when oauth is enabled alongside advanced config' do
 		let(:manifest_properties) { {
 				'rabbitmq-server' => {
           'oauth' => {
@@ -65,13 +42,19 @@ B9q9KFBmo4Ahh/6+d4wM1rH9kxl0RvMAKLe+daoIHIjok8hCO4cKQQEw/ErBe4SF
 QAvkknWitpRK8KVLypEj5WKej6CF8nq30utn15FQg0JkHoqzwiCqqeen8GIPteI7
 VwIDAQAB
 -----END PUBLIC KEY-----"
-          }
+          },
+          'override_advanced_config': 'WwogIHtyYWJiaXQsIFsKICAgICAge3RjcF9saXN0ZW5lcnMsIFs1NjczXX0KICAgIF0KICB9Cl0u'
 		    }
       } 
     }
 
-		it 'configures only the oauth advanced config that does not have a Cuttlefish equivalent' do
-      expect(output).to eq(File.open("spec/unit/templates/assets/expected_rabbitmq_advanced.config").read.strip)
+		it 'configures no oAuth config, and still configures the override advanced config' do
+			expect(output).to eq('[
+  {rabbit, [
+      {tcp_listeners, [5673]}
+    ]
+  }
+].')
     end
 	end
 end
