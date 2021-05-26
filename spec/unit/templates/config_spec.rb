0247e8cc34d4c8c04bf302245cf8f878f55a9980
Connor Rogers <rconnor@vmware.com>
2021-05-26 12:33:20 +0000

    Allow TLS communication between nodes

--- spec/unit/templates/config_spec.rb
+++ spec/unit/templates/config_spec.rb
@@ -3,12 +3,46 @@ require 'bosh/template/renderer'
 
 RSpec.describe 'Configuration', template: true do
   let(:rendered_template) {
-    compiled_template('rabbitmq-server', 'config')
+    compiled_template('rabbitmq-server', 'config', manifest_properties)
   }
 
   describe 'environment variables' do
+    let(:manifest_properties) do
+      {
+        'rabbitmq-server' => {}
+      }
+    end
     it 'exports a RMQ_FD_LIMIT to set the global ulimit' do
       expect(rendered_template).to include("RMQ_FD_LIMIT=300000")
+    end
+  end
+
+  context 'internode TLS is enabled' do
+    let(:manifest_properties) do
+      {
+        'rabbitmq-server' => {
+          'ssl' => {
+            'inter_node_enabled' => true
+          }
+        }
+      }
+    end
+    context 'TLS is disabled' do
+      before do
+        manifest_properties['rabbitmq-server']['ssl']['enabled'] = false
+      end
+      it 'does not contain erl args for rabbitmqctl' do
+        expect(rendered_template).not_to include('RABBITMQ_CTL_ERL_ARGS="-proto_dist inet_tls -ssl_dist_optfile /var/vcap/jobs/rabbitmq-server/etc/inter_node_tls.config"')
+      end
+    end
+
+    context 'TLS is enabled' do
+      before do
+        manifest_properties['rabbitmq-server']['ssl']['enabled'] = true
+      end
+      it 'does contain erl args for rabbitmqctl' do
+        expect(rendered_template).to include('RABBITMQ_CTL_ERL_ARGS="-proto_dist inet_tls -ssl_dist_optfile /var/vcap/jobs/rabbitmq-server/etc/inter_node_tls.config"')
+      end
     end
   end
 end
