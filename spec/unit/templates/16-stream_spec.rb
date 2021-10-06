b20dce82441ce2b405bc0c4bac27f7324a38497e
Michal Kuratczyk <mkuratczyk@vmware.com>
2021-10-06 13:09:35 +0200

    Fix: don't configure Stream listener when the plugin is disabled

--- spec/unit/templates/16-stream_spec.rb
+++ spec/unit/templates/16-stream_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe 'Configuration', template: true do
     end
     it 'renders stream config' do
       expect(rendered_template).to include('stream.listeners.ssl.default = 5551')
+      expect(rendered_template).not_to include('stream.listeners.tcp = none')
     end
 
     context 'when disable_non_ssl_listeners is true' do
@@ -32,8 +33,32 @@ RSpec.describe 'Configuration', template: true do
         manifest_properties['rabbitmq-server']['ssl']['disable_non_ssl_listeners'] = true
       end
       it 'disables Stream TCP listeners' do
+        expect(rendered_template).to include('stream.listeners.ssl.default = 5551')
         expect(rendered_template).to include('stream.listeners.tcp = none')
       end
+    end
+  end
+
+  context 'when rabbitmq_stream plugin is disabled' do
+    let(:manifest_properties) do
+      {
+        'rabbitmq-server' => {
+          'plugins' => [''],
+          'ssl' => {
+            'enabled' => true,
+            'cacert' => 'fake CA cert',
+            'cert' => 'fake cert',
+            'key' => 'fake key',
+            'verification_depth' => 3,
+            'versions' => ['tlsv1.2', 'tlsv1.1'],
+            'disable_non_ssl_listeners' => true
+          }
+        }
+      }
+    end
+
+    it 'stream plugin is not configured' do
+      expect(rendered_template).not_to include('stream.listeners')
     end
   end
 end
