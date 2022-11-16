69e75b5fbbd25c464f9986b3fedc0e6d3136ab2b
Mirah Gary <mgary@vmware.com>
2022-11-16 12:35:07 +0100

    Bump dependencies to resolve merge conflicts.

--- src/rabbitmq-admin/amqp-traffic/drop-amqp-traffic/drop_amqp_traffic_suite_test.go
+++ src/rabbitmq-admin/amqp-traffic/drop-amqp-traffic/drop_amqp_traffic_suite_test.go
@@ -3,11 +3,23 @@ package main_test
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
 )
 
 func TestDropAmqpTraffic(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "DropAmqpTraffic Suite")
 }
+
+var pathToCmd string
+var _ = BeforeSuite(func() {
+	var err error
+	pathToCmd, err = Build("rabbitmq-admin/amqp-traffic/drop-amqp-traffic")
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	CleanupBuildArtifacts()
+})
--- src/rabbitmq-admin/amqp-traffic/restore-amqp-traffic/restore_amqp_traffic_suite_test.go
+++ src/rabbitmq-admin/amqp-traffic/restore-amqp-traffic/restore_amqp_traffic_suite_test.go
@@ -3,11 +3,24 @@ package main_test
 import (
 	"testing"
 
-	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
 )
 
 func TestDropAmqpTraffic(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "RestoreAmqpTraffic Suite")
 }
+
+var pathToCmd string
+
+var _ = BeforeSuite(func() {
+	var err error
+	pathToCmd, err = Build("rabbitmq-admin/amqp-traffic/restore-amqp-traffic")
+	Expect(err).NotTo(HaveOccurred())
+})
+
+var _ = AfterSuite(func() {
+	CleanupBuildArtifacts()
+})
