5d389014fe6f32837d44c5be00f9263592c6e248
Mirah Gary <mgary@vmware.com>
2023-05-10 14:04:01 +0200

    Split feature flag enablement across cases.

--- jobs/rabbitmq-server/templates/enable-feature-flags.bash
+++ jobs/rabbitmq-server/templates/enable-feature-flags.bash
@@ -5,12 +5,25 @@
 set -u
 
 main() {
-    enable_all
+    enable_all_3_8_features
+    enable_all_supported_features
     enable_stream_queue
 }
 
-enable_all() {
-    enable_feature_flag "all"
+enable_all_3_8_features() {
+    for feature in "implicit_default_bindings" "mainenance_mode_status" "quourum_queue" "user_limits" "virtual_host_metadata"
+    do
+        enable_feature_flag "$feature"
+    done
+}
+
+enable_all_supported_features() {
+    for feature in "classic_morrored_queue_version" "classic_queue_type_delivery_support" "drop_unroutable_metric" "empty_basic_get_metric"
+    do
+        if feature_flag_supported "$feature"; then
+            enable_feature_flag "$feature"
+        fi
+    done
 }
 
 enable_stream_queue() {
